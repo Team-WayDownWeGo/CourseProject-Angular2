b1ceb309470080c37c73e8780f6bb601dce37fd0
martinst1 <martinst@gmail.com>
2016-12-28 12:29:31 +0200

    fix

--- src/app/guard/guard.ts
+++ src/app/guard/guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
+
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
