2e7c3cf89ee3832769c992d00d0fc000a5c6a13a
martinst1 <martinst@gmail.com>
2016-12-30 01:23:03 +0200

    fix

--- src/app/users/public-profile/public-profile/public-profile.component.ts
+++ src/app/users/public-profile/public-profile/public-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { UserService } from './../../../common-services/user.service';
 import { User } from './../../model/user-model';
@@ -15,17 +15,19 @@ import { NotificationsService } from './../../../../../node_modules/angular2-not
   styleUrls: ['./public-profile.component.css']
 })
 export class PublicProfileComponent implements OnInit {
-  private _route;
-  private _params;
+  private _route: ActivatedRoute;
+  private _params: any;
   private _notificationService: NotificationsService;
-  private _username;
-  public _userService;
+  private _username: string;
+  private _router: Router;
+  public _userService: UserService;
   public username: string;
   public firstname: string;
   public lastname: string;
   // for test purposes to replace with real bool when created
   public isUserLoggedIn: boolean;
   public message: Message;
+  public displayHeader: Boolean;
 
   public options = {
     position: ['bottom', 'right'],
@@ -33,13 +35,15 @@ export class PublicProfileComponent implements OnInit {
     lastOnBottom: true
   }
 
-  constructor(route: ActivatedRoute, userService: UserService, notificationService: NotificationsService) {
+  constructor(route: ActivatedRoute, userService: UserService, notificationService: NotificationsService, router: Router) {
     this._route = route;
     this._params = route.params;
     this._userService = userService;
     this.message = new Message();
     this._notificationService = notificationService;
     this._username = this._params._value.username;
+    this.displayHeader = true;
+    this._router = router;
   }
 
   private getUser() {
@@ -48,10 +52,18 @@ export class PublicProfileComponent implements OnInit {
     this._userService.getUserData(username)
       .subscribe(
       user => {
+        if (user) {
+          this.username = user.username;
+          this.firstname = user.firstName;
+          this.lastname = user.lastName;
+        }
+        else {
+          // ne raboti :)
+          this._notificationService.error("test", "test");
+          this._router.navigateByUrl('/');
+        }
         // TODO: find a way to work directly with observable ?????
-        this.username = user.username;
-        this.firstname = user.firstName;
-        this.lastname = user.lastName;
+
       },
       // TODO: redirect to users not found html page
       error => console.log('not found'));
@@ -71,6 +83,7 @@ export class PublicProfileComponent implements OnInit {
             clickToClose: false,
             maxLength: 10
           });
+        this.displayHeader = !this.displayHeader;
         console.log(response.id);
       },
       err => console.log(err));
