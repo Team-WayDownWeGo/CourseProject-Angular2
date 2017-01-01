4bffe235c66185cf9efa7570a9682c4365f56eaa
BoykoBonev <boykobonev@outlook.com>
2017-01-01 13:02:22 +0200

    Sort for search page complete

--- src/app/forum/list-posts/post-row/post-row.component.ts
+++ src/app/forum/list-posts/post-row/post-row.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostRowComponent implements OnInit {
 
+  @Input() params: any;
   @Input() post: any;
   @Input() sortParam: any;
   constructor() { }
