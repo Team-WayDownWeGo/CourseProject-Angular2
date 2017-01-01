4bc2b35a314291d683dfb8cb8516dcda95fce4d9
martinst1 <martinst@gmail.com>
2017-01-01 23:19:26 +0200

    adding profile home

--- src/app/profile/profile-home/profile-home.component.ts
+++ src/app/profile/profile-home/profile-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './../../common-services/user.service';
+import { ForumService } from './../../services/forum.service';
 
 @Component({
   selector: 'app-profile-home',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileHomeComponent implements OnInit {
 
-  constructor() { }
+  private _userService: UserService;
+  public userDetails: any;
+  private _forumService: ForumService;
+  public username: any = JSON.parse(localStorage.getItem('user')).result.username;
+  public likes: number = 0;
+  public posts: number = 0;
 
-  ngOnInit() {
+  constructor(userService: UserService, forumService: ForumService) {
+    this._userService = userService;
+    this._forumService = forumService;
+    this.userDetails = '';
   }
-
+  private getUserDetails() {
+    this._userService.getUserData(this.username).subscribe(user => {
+      this.userDetails = user;
+    });
+  }
+  private getNumberOfLikes() {
+    this._forumService.getAllUserPosts(this.username).subscribe(allPosts => {
+      allPosts.forEach(p => {
+        this.posts++;
+        this.likes += p.likes;
+      });
+    });
+  }
+  ngOnInit() {
+    this.getUserDetails();
+    this.getNumberOfLikes();
+  }
 }
